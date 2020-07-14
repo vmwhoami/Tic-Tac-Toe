c664bf324bee3e0c8fb9aad055498dd2e791f4a3
Somoye123 <somoye.ayotunde@gmail.com>
2020-07-14 13:18:59 -0700

    add display_board, update_board, win and board_slot_completed methods to board class

--- lib/board.rb
+++ lib/board.rb
@@ -1,38 +1,83 @@
 class Board
-    @@win_scenario =  [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [6, 4, 2]
-      ]
-    def initialize(first,second)
-        @board = %w[1 2 3 4 5 6 7 8 9]
-        @max_number_of_input = 9
-        @first_player = first
-        @second_player = second
-    end
 
-    def display_board
-      separator = '|'
-  
-     puts "\n| #{@board[0]} #{separator} #{@board[1]} #{separator} #{@board[2]} |
+  attr_accessor :highest_turns
+
+  @@win_scenario = [
+
+    [0, 1, 2],
+
+    [3, 4, 5],
+
+    [6, 7, 8],
+
+    [0, 3, 6],
+
+    [1, 4, 7],
+
+    [2, 5, 8],
+
+    [0, 4, 8],
+
+    [6, 4, 2]
+
+  ]
+
+  def initialize(first, second)
+
+    @board = %w[1 2 3 4 5 6 7 8 9]
+
+    @highest_turns = 9
+
+    @first_player = first
+
+    @second_player = second
+
+  end
+
+  def display_board
+
+    separator = '|'
+
+    puts "\n| #{@board[0]} #{separator} #{@board[1]} #{separator} #{@board[2]} |
+
        \n| #{@board[3]} #{separator} #{@board[4]} #{separator} #{@board[5]} |
+
        \n| #{@board[6]} #{separator} #{@board[7]} #{separator} #{@board[8]} | "
+
+  end
+
+  def update_board(current_player, board_number)
+
+    @board[board_number - 1] = current_player == @first_player ? 'x' : 'o'
+
+  end
+
+  def win?
+
+    @@win_scenario.each do |win_array|
+
+      first_index = win_array[0]
+
+      second_index = win_array[1]
+
+      third_index = win_array[2]
+
+      return true if @board[first_index] == @board[second_index] && @board[first_index] == @board[third_index]
+
     end
 
-    def update_board(current_player,option_selected)
-    current_player == @first_player ? @board[option_selected - 1] = 'v' : @board[option_selected - 1] = 's'
+    false
+
+  end
+
+  def board_slot_completed?
+    
+    if @highest_turns < 1
+      puts 'it is a draw'
+      return true
     end
-     
-    def win?
-    end
+    false
+  end
 
 end
 
-
-test = Board.new
-test.display_board
